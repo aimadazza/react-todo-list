8944d8c8a19bd1ca526f9befa9108cc1ebae1661
aimad <marrakechdat@gmail.com>
2020-07-21 21:08:25 +0000

    showing vs-code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './componnents/TodoInput'
 import TodoList from './componnents/TodoList'
-
+import { v4 as uuidv4 } from 'uuid';
 export default class App extends Component {
+  state={
+    items:[],
+    item:'',
+    id:uuidv4(),
+  }
   render() {
     return (
       <div>
